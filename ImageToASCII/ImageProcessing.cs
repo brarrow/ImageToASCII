0c40a5d9928131e60ba22cbdf9c569979601bf72
HomeHarbor\greenbarrow <s.naumoff@yahoo.com>
2017-09-23 01:16:41 +0300

    Added project

---
+++ ImageToASCII/ImageProcessing.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace pngtoascii
+{
+    static class ImageProcessing
+    {
+        //begin for convert bitmap to imagesource
+        [DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject([In] IntPtr hObject);
+
+        //end
+
+        public static byte[] GetBytesFromBitmap(Bitmap image)
+        {
+            // Lock the bitmap's bits.
+
+            BitmapData bits = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, image.PixelFormat);
+            IntPtr ptr = bits.Scan0;
+            // Declare an array to hold the bytes of the bitmap.
+            int numbytes = bits.Stride * image.Height;
+            byte[] bytes = new byte[numbytes];
+            // Copy the RGB values into the array.
+            Marshal.Copy(ptr, bytes, 0, numbytes);
+            // Unlock the bits.
+            image.UnlockBits(bits);
+            return bytes;
+        }
+
+        public static void SetBytesToBmp(Bitmap image, byte[] bytes)
+        {
+            BitmapData bits = image.LockBits(new System.Drawing.Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, image.PixelFormat);
+            IntPtr ptr = bits.Scan0;
+            int numbytes = bits.Stride * image.Height;
+            bits = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.WriteOnly, image.PixelFormat);
+            ptr = bits.Scan0;
+            // Copy the array of color values into bitmap
+            Marshal.Copy(bytes, 0, ptr, numbytes);
+            // Unlock the bits.
+            image.UnlockBits(bits);
+
+        }
+
+        public static Bitmap MakeGray(Bitmap image)
+        {
+            //create a blank bitmap the same size as original
+            Bitmap newBitmap = new Bitmap(image.Width, image.Height);
+
+            //get a graphics object from the new image
+            Graphics g = Graphics.FromImage(newBitmap);
+
+            //create the grayscale ColorMatrix
+            ColorMatrix colorMatrix = new ColorMatrix(
+            new float[][]
+            {
+                new float[] {.3f, .3f, .3f, 0, 0},
+                new float[] {.59f, .59f, .59f, 0, 0},
+                new float[] {.11f, .11f, .11f, 0, 0},
+                new float[] {0, 0, 0, 1, 0},
+                new float[] {0, 0, 0, 0, 1}
+            });
+
+            //create some image attributes
+            ImageAttributes attributes = new ImageAttributes();
+
+            //set the color matrix attribute
+            attributes.SetColorMatrix(colorMatrix);
+
+            //draw the original image on the new image
+            //using the grayscale color matrix
+            g.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height),
+               0, 0, image.Width, image.Height, GraphicsUnit.Pixel, attributes);
+
+            //dispose the Graphics object
+            g.Dispose();
+            return newBitmap;
+        }
+
+        public static Bitmap ResizeBitmap(Bitmap sourceBMP, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(result))
+                g.DrawImage(sourceBMP, 0, 0, width, height);
+            return result;
+        }
+
+        public static Bitmap ConvertPixelFormat(Bitmap image)
+        {
+            Bitmap clone = new Bitmap(image.Width, image.Height,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            using (Graphics gr = Graphics.FromImage(clone))
+            {
+                gr.DrawImage(image, new Rectangle(0, 0, clone.Width, clone.Height));
+            }
+            return clone;
+        }
+
+        public static ImageSource ImageSourceForBitmap(Bitmap image)
+        {
+            var handle = image.GetHbitmap();
+            try
+            {
+                return Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally { DeleteObject(handle); }
+        }
+
+       // public s
+    }
+}
--- ImageToASCII/MainWindow.xaml.cs
+++ ImageToASCII/MainWindow.xaml.cs
@@ -1,28 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Drawing;
+using System.IO;
+using System.Diagnostics;
 
-namespace ImageToASCII
+namespace pngtoascii
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        string filename;
+        Bitmap bmporig;
+        byte[] data;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void bt_loadfile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                tb_openedimage.Text = filename = openFileDialog.FileName;
+            bmporig = ImageProcessing.ConvertPixelFormat(new Bitmap(filename));
+            //bmpimage.p
+            bmp_image.Source = ImageProcessing.ImageSourceForBitmap(bmporig);
+        }
+
+        private char[] getcharsfromtb()
+        {
+            return tb_symbols.Text.ToCharArray();
+        }
+
+        private void bt_bmptoascii_Click(object sender, RoutedEventArgs e)
+        {
+            Bitmap bmpused = bmporig;
+            if (rb_BOLKNOT.IsChecked ?? false)
+            {
+                int newwidth = 100;
+                int newheidht = Convert.ToInt32(Math.Round(Convert.ToDouble(newwidth) / bmporig.Width * bmporig.Height));
+                bmpused = ImageProcessing.ResizeBitmap(bmporig, newwidth, newheidht);
+            }
+            char[] chars = getcharsfromtb();
+            data = ImageProcessing.GetBytesFromBitmap(bmpused);
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(filename.Remove(filename.LastIndexOf('\\')) + @"\Result.txt"))
+                {
+                    int j = 0;
+                    int buf=0;
+                    int symbol=0;
+                    int el = 255 * 3 / chars.Length;
+                    for (int i = 0; i < data.Length - 3; i +=4)
+                    {
+                        if (j == bmpused.Width-1)
+                        {
+                            sw.WriteLine();
+                            j = 0;
+                        }else j++;
+
+                        buf = data[i] + data[i+1] + data[i + 2];
+                        symbol = buf / el - 1;
+                        if (symbol < 0) symbol = 0;
+                        sw.Write(chars[symbol]);
+                    }
+
+                }
+            }
+            catch { };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Process PrFolder = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo();
+            string file = filename.Remove(filename.LastIndexOf('\\')+1) + "Result.txt";
+            psi.CreateNoWindow = true;
+            psi.WindowStyle = ProcessWindowStyle.Normal;
+            psi.FileName = "explorer";
+            psi.Arguments = @"/n, /select, " + file;
+            PrFolder.StartInfo = psi;
+            PrFolder.Start();
         }
     }
 }
